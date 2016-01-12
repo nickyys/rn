a30c6c9e0143b2234cf91069cb51dc5a27cc25f8
Nicky <nickyys@21cn.com>
2016-01-12 14:13:36 +0800

    demo movies

--- index.android.js
+++ index.android.js
@@ -3,48 +3,135 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, {
+var React = require('react-native');
+var {
   AppRegistry,
-  Component,
+  ToolbarAndroid,
+  Image,
+  ListView,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+} = React;
 
-class test extends Component {
-  render() {
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
+
+var test = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+
+  //渲染函数
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <View style={{flex: 1}}>
+        <ToolbarAndroid
+            style={styles.toolbar}
+            titleColor="white"
+            title="demo"
+        />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderMovie}
+          style={styles.listView}
+        />
+      </View>
+    );
+  },
+
+  //视图1加载中
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Text>
+          Loading movies...
         </Text>
       </View>
     );
-  }
-}
+  },
 
-const styles = StyleSheet.create({
+  //视图2显示列表
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>上映时间：{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
+    left:10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  title: {
+    fontSize: 18,
+    marginBottom: 8,
+    textAlign: 'left',
+  },
+  year: {
+    fontSize: 14,
+    textAlign: 'left',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+    borderWidth: 0.5,
+    borderColor: 'black'
+  },
+  listView: {
+    paddingTop: 0,
+    backgroundColor: '#F5FCFF',
+  },
+  toolbar: {
+    backgroundColor: '#a9a9a9',
+    height: 56,
   },
 });
 
